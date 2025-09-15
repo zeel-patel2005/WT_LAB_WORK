9c7dc41cdebc514bf3e9b8102f14786c1a4844d9
zeel patel <zeelp4797@gmail.com>
2025-09-15 19:32:44 +0530

    add the crudexp6

---
+++ C#.Net/CRUD_EXP6/CRUD_EXP6/WebForm1.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="CRUD_EXP6.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+     <form id="form1" runat="server">
+    <div style="width:600px; margin:auto;">
+
+        <h2>Manage Employees</h2>
+
+        <asp:Label ID="lblMessage" runat="server" ForeColor="Green"></asp:Label><br /><br />
+
+        <asp:Label ID="Label1" runat="server" Text="Name:"></asp:Label>
+        <asp:TextBox ID="txtName" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Label ID="Label2" runat="server" Text="Position:"></asp:Label>
+        <asp:TextBox ID="txtPosition" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Label ID="Label3" runat="server" Text="Salary:"></asp:Label>
+        <asp:TextBox ID="txtSalary" runat="server"></asp:TextBox><br /><br />
+
+        <asp:HiddenField ID="hfId" runat="server" />
+
+        <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+        <asp:Button ID="btnClear" runat="server" Text="Clear" OnClick="btnClear_Click" /><br /><br />
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" DataKeyNames="Id"
+            OnSelectedIndexChanged="GridView1_SelectedIndexChanged">
+            <Columns>
+                <asp:BoundField DataField="Id" HeaderText="ID" ReadOnly="True" />
+                <asp:BoundField DataField="Name" HeaderText="Name" />
+                <asp:BoundField DataField="Position" HeaderText="Position" />
+                <asp:BoundField DataField="Salary" HeaderText="Salary" />
+                <asp:CommandField ShowSelectButton="True" SelectText="Select" />
+            </Columns>
+        </asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ C#.Net/CRUD_EXP6/CRUD_EXP6/WebForm1.aspx.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD_EXP6
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        string cs = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadData();
+            }
+        }
+
+        // Load data into GridView
+        private void LoadData()
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Employees", con);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+            }
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(cs))
+            {
+                string query = "INSERT INTO Employees(Name,Position,Salary) VALUES(@Name,@Position,@Salary)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                cmd.Parameters.AddWithValue("@Position", txtPosition.Text);
+                cmd.Parameters.AddWithValue("@Salary", txtSalary.Text);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
+                con.Close();
+                lblMessage.Text = "Record Inserted!";
+                LoadData();
+                ClearFields();
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(hfId.Value))
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    string query = "UPDATE Employees SET Name=@Name,Position=@Position,Salary=@Salary WHERE Id=@Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Position", txtPosition.Text);
+                    cmd.Parameters.AddWithValue("@Salary", txtSalary.Text);
+                    cmd.Parameters.AddWithValue("@Id", hfId.Value);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    lblMessage.Text = "Record Updated!";
+                    LoadData();
+                    ClearFields();
+                }
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(hfId.Value))
+            {
+                using (SqlConnection con = new SqlConnection(cs))
+                {
+                    string query = "DELETE FROM Employees WHERE Id=@Id";
+                    SqlCommand cmd = new SqlCommand(query, con);
+                    cmd.Parameters.AddWithValue("@Id", hfId.Value);
+
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    lblMessage.Text = "Record Deleted!";
+                    LoadData();
+                    ClearFields();
+                }
+            }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // When user clicks Select in GridView, load data into textboxes
+            GridViewRow row = GridView1.SelectedRow;
+            hfId.Value = row.Cells[0].Text;
+            txtName.Text = row.Cells[1].Text;
+            txtPosition.Text = row.Cells[2].Text;
+            txtSalary.Text = row.Cells[3].Text;
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            txtName.Text = "";
+            txtPosition.Text = "";
+            txtSalary.Text = "";
+            hfId.Value = "";
+        }
+    }
+}
