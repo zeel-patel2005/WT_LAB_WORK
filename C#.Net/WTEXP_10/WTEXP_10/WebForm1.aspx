90fe4b2c071d7c00db81eca54c2b34115dbd0853
zeel patel <zeelp4797@gmail.com>
2025-09-29 22:14:23 +0530

    add exp10.2

---
+++ C#.Net/WTEXP_10/WTEXP_10/WebForm1.aspx
@@ -0,0 +1,25 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="WebForm1.aspx.cs" Inherits="WTEXP_10.WebForm1" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cache Dependency Demo (XML + SQL)</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="font-family: Arial; padding:20px;">
+            <h2>Zeel Cache Dependency Demo</h2>
+            
+            <!-- XML File Dependency -->
+            <h3>XML File Dependency</h3>
+            <asp:Label ID="lblXmlCache" runat="server" Text=""></asp:Label><br />
+            <asp:Button ID="btnXmlCache" runat="server" Text="Load XML Cache" OnClick="btnXmlCache_Click" /><br /><br />
+
+            <!-- SQL Database Dependency -->
+            <h3>SQL Database Dependency</h3>
+            <asp:Label ID="lblSqlCache" runat="server" Text=""></asp:Label><br />
+            <asp:Button ID="btnSqlCache" runat="server" Text="Load SQL Cache" OnClick="btnSqlCache_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ C#.Net/WTEXP_10/WTEXP_10/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Caching;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WTEXP_10
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnXmlCache_Click(object sender, EventArgs e)
+        {
+            string cacheKey = "xmlData";
+
+            if (Cache[cacheKey] == null)
+            {
+                string filePath = Server.MapPath("~/App_Data/data.xml");
+
+                CacheDependency dependency = new CacheDependency(filePath);
+
+                Cache.Insert(cacheKey, DateTime.Now.ToString(), dependency);
+
+                lblXmlCache.Text = "XML Cache created at: " + Cache[cacheKey];
+            }
+            else
+            {
+                lblXmlCache.Text = "XML Cache available: " + Cache[cacheKey];
+            }
+        }
+
+        // SQL Database Dependency
+        protected void btnSqlCache_Click(object sender, EventArgs e)
+        {
+            string cacheKey = "studentData";
+
+            if (Cache[cacheKey] == null)
+            {
+                SqlCacheDependency dependency = new SqlCacheDependency("WTEXP_10", "Students");
+
+                string data = "SQL Cache created at " + DateTime.Now.ToString();
+                Cache.Insert(cacheKey, data, dependency);
+
+                lblSqlCache.Text = "SQL Cache created: " + data;
+            }
+            else
+            {
+                lblSqlCache.Text = "SQL Cache available: " + Cache[cacheKey];
+            }
+        }
+    }
+}
